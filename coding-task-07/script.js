03d3f21c4ca01450919f51ceaee5e9ec5e8ceed9
Robin Wuebbenhorst <r.wuebbenhorst@web.de>
2024-05-07 19:08:24 +0200

    07-arrays

---
+++ coding-task-07/script.js
@@ -0,0 +1,17 @@
+// 01) removeItem function
+
+// TODO: Implement the removeItem function
+
+const ainmals = ["Dog", "Cat", "Lion"];
+console.log(removeItem(ainmals, 1));
+// result should be: ["Cat", "Lion"]
+
+console.log(ainmals);
+// result should be still: ["Dog", "Cat", "Lion"]
+
+const fruits = ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"];
+console.log(removeItem(fruits, 3));
+// result should be: ["Watermelon", "Banana", "Kiwi", "Pineapple", "Apple"]
+
+console.log(fruits);
+// result should be still: ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"]
