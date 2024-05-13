d33072fbbcaa7a30d4876dbe20a021e49f59590a
Robin Wuebbenhorst <r.wuebbenhorst@web.de>
2024-05-13 14:00:52 +0200

    06

--- coding-task-07/script.js
+++ coding-task-07/script.js
@@ -1,6 +1,14 @@
 // 01) removeItem function
 
 // TODO: Implement the removeItem function
+function removeItem(array, index) {
+  if (index < 1 || index > array.length) {
+    return [...array];
+  }
+  const newArray = [...array];
+  newArray.splice(index - 1, 1);
+  return newArray;
+}
 
 const ainmals = ["Dog", "Cat", "Lion"];
 console.log(removeItem(ainmals, 1));
@@ -15,3 +23,44 @@ console.log(removeItem(fruits, 3));
 
 console.log(fruits);
 // result should be still: ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"]
+
+// 03) sumOfCharacters function
+
+function sumOfCharacters(array) {
+  let totalCharacters = 0;
+  array.forEach((item) => {
+    if (typeof item === "string") {
+      totalCharacters += item.length;
+    }
+  });
+  return totalCharacters;
+}
+// TODO: Implement the sumOfCharacters function
+
+const arr1 = ["Luke", "Anakin", true, "Obi Wan", 333];
+console.log(sumOfCharacters(arr1));
+// result should be: 17
+
+const arr2 = [
+  "Code is",
+  "like humor",
+  ".",
+  "When you have",
+  "to explain it, it's bad!",
+];
+console.log(sumOfCharacters(arr2));
+// result should be: 55
+
+// objects test
+
+const userData = {
+  firstname: "Robin",
+  surname: "Wübbenhorst",
+  adress: {
+    street: "Test",
+    postalcode: "123456",
+    country: "Germany",
+  },
+};
+
+console.log(userData);
